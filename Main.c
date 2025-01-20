e26415652f5dbc4e9b6547115a01081fb5307a3d
Max0xDay <backup.day28@gmail.com>
2025-01-20 10:18:19 +0200

    Migrating to new format

--- Main.c
+++ Main.c
@@ -1,6 +1,7 @@
 /*
  *   Author: Max Day
- *   Description: c test pin thing ill work it out later
+ *   Description: BSI Pin voltage drop system based on api
+ *                System will read in voltages using a single channel Optocoupler voltage detection sensor measuign high or low voltages to test if the system is on or not.
  *
  */
 
@@ -70,6 +71,16 @@ when entering into critical state
 
     work out why firefox just dies when server side is loaded 
 
+*/
+
+/*
+//////////////////////////  UPDATE 20/01/2025   //////////////////////////  
+
+
+This code will be a proof of concept that will only test of the pins are high or low (on or off). status requests will be send through the 
+return json to keep it simple and not require 2 API's or worse a websocket
+
+Server side needs to be rewirtten to graph and show data better for when its dropped. also need to make it so we can export jsons.
 
 */
 
@@ -95,7 +106,7 @@ when entering into critical state
 #define STATUS_INTERVAL 2 // Seconds between status updates
 #define COMMAND_CHECK_INTERVAL 1
 
-const int PIN[] = {0,2,3,7,8,9,12,13,14,21};
+const int PIN[] = {0,2,3,7,8};
 
 //TODO: modify it so packages all follow similar structure 
 struct ResponseData {
@@ -123,9 +134,8 @@ int send_data_request(const int status, float PIN[]) { //TODO: clean this up
   // Prepare payload
   char payload[512];
   snprintf(payload, sizeof(payload),
-    "{\"timestamp\":\"%s\",\"status\":%d,\"pin0\":%.2f,\"pin2\":%.2f,\"pin3\":%.2f,\"pin7\":%.2f,\"pin8\":%.2f,\"pin9\":%.2f,"
-    "\"pin12\":%.2f,\"pin13\":%.2f,\"pin14\":%.2f,\"pin21\":%.2f}",
-    timestamp, status, PIN[0], PIN[1], PIN[2], PIN[3], PIN[4], PIN[5], PIN[6], PIN[7], PIN[8], PIN[9]);
+    "{\"timestamp\":\"%s\",\"status\":%d,\"pin0\":%.2f,\"pin2\":%.2f,\"pin3\":%.2f,\"pin7\":%.2f,\"pin8\":%.2f}",
+    timestamp, status, PIN[0], PIN[1], PIN[2], PIN[3], PIN[4]);
 
   curl = curl_easy_init();
   if (curl) {
@@ -157,6 +167,9 @@ int send_data_request(const int status, float PIN[]) { //TODO: clean this up
  * - 1: Critical
  * - 2: System OK
  */
+
+
+
 int send_status_update(int state) {
   CURL * curl = curl_easy_init();
   if (curl) {
@@ -176,50 +189,89 @@ int send_status_update(int state) {
   return 0;
 }
 
+
+int send_state_request(int state) {
+  CURL * curl;
+  CURLcode res;
+  int http_code = 0;
+
+  char payload[50];
+  snprintf(payload, sizeof(payload), "{\"state\": %d}", state);
+
+  curl = curl_easy_init();
+  if (curl) {
+    struct curl_slist * headers = NULL;
+    headers = curl_slist_append(headers, "Content-Type: application/json");
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+    curl_easy_setopt(curl, CURLOPT_URL, ({
+      char url[256];snprintf(url, sizeof(url), "%s/system/state", API_ENDPOINT);url;
+    }));
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, payload);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+
+    res = curl_easy_perform(curl);
+    if (res == CURLE_OK) {
+      curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+    }
+
+    curl_slist_free_all(headers);
+    curl_easy_cleanup(curl);
+  }
+  return http_code;
+}
+
+
+
+
+
 int main() {
+    time_t last_send_time = 0;
+    time_t last_status_time = 0;
+    int previous_state[5] = {0};
 
-  time_t last_check = 0;
-  time_t last_status_update = 0;
-
-  // WiringPi setup
-  if (wiringPiSetup() == -1) {
-    fprintf(stderr, "WiringPi setup failed\n");
-    return 1;
-  }
-
-  // Set pin modes
-  
-  for (int i = 0; i < sizeof(PIN) / sizeof(PIN[0]); i++) {
-    pinMode(PIN[i], INPUT);
-  }
-  printf("Voltage monitoring started...\n");
-
-  while (1) {
-    time_t now = time(NULL);
-
-
-    if (now - last_check >= CHECK_INTERVAL) {
-      last_check = now;
-      int current_state = digitalRead(PIN[0]);
-      const int status = (current_state == LOW) ? 1 : 2;
-
-      // Read pin values
-      float pin_values[10];
-      for (int i = 0; i < sizeof(PIN) / sizeof(PIN[0]); i++) {
-        pin_values[i] = digitalRead(PIN[i]);
-      }
-
-      send_data_request(status, pin_values);
+    if (wiringPiSetup() == -1) {
+        fprintf(stderr, "WiringPi setup failed\n");
+        return 1;
     }
 
-    if (now - last_status_update >= STATUS_INTERVAL) {
-      last_status_update = now;
-      int state = ((digitalRead(PIN[0])) == LOW) ? 1 : 2;
-      send_status_update(state);
+    for (int i = 0; i < sizeof(PIN) / sizeof(PIN[0]); i++) {
+        pinMode(PIN[i], INPUT);
+        previous_state[i] = digitalRead(PIN[i]);
+    }
+    printf("Voltage monitoring started...\n");
+
+    int critical_state = 2;
+    while (1) {
+        time_t now = time(NULL);
+        float pin_values[5];
+        int new_critical_state = 2;
+
+        for (int i = 0; i < sizeof(PIN) / sizeof(PIN[0]); i++) {
+            pin_values[i] = digitalRead(PIN[i]);
+            if (previous_state[i] == HIGH && pin_values[i] == LOW) {
+                new_critical_state = 1;
+            }
+            previous_state[i] = pin_values[i];
+        }
+
+        if (new_critical_state != critical_state) {
+            critical_state = new_critical_state;
+            send_status_update(critical_state);
+            send_data_request(critical_state, pin_values);
+        }
+
+        if (now - last_send_time >= CHECK_INTERVAL) {
+            last_send_time = now;
+            send_data_request(critical_state, pin_values);
+        }
+
+        if (now - last_status_time >= STATUS_INTERVAL) {
+            last_status_time = now;
+            send_status_update(critical_state);
+        }
+
+        sleep(1);
     }
 
-    sleep(1);
-  }
-
-  return 0;
+    return 0;
 }
